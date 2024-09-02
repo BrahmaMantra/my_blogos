31971861329507d47ed6fb13fee39698849b3471
BrahmaMantra <2033552517@qq.com>
2024-09-02 17:43:09 +0800

    实现了简单的堆分配管理

--- src/lib.rs
+++ src/lib.rs
@@ -13,6 +13,8 @@ pub mod serial;
 pub mod vga_buffer;
 pub mod gdt;
 pub mod memory;
+pub mod allocator;
+extern crate alloc;
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 #[repr(u32)]
 pub enum QemuExitCode {
--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,13 @@
 #![test_runner(blog_os::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
-use blog_os::{memory::{self, BootInfoFrameAllocator}, println};
+use alloc::{boxed::Box, rc::Rc, vec::Vec,vec};
+use blog_os::{allocator, memory::{self, BootInfoFrameAllocator}, println};
 use bootloader::{entry_point, BootInfo};
 use x86_64::structures::paging::{Page, Translate};
 use ::x86_64::VirtAddr;
 use core::panic::PanicInfo;
+extern crate alloc;
 
 entry_point!(kernel_main);
 
@@ -21,14 +23,34 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
     let mut frame_allocator = unsafe {
         BootInfoFrameAllocator::init(&boot_info.memory_map)
     };
+    allocator::init_heap(&mut mapper, &mut frame_allocator)
+    .expect("heap initialization failed");
 
-    // 映射未使用的页
-    let page = Page::containing_address(VirtAddr::new(0xdeadbeaf000));
-    memory::create_example_mapping(page, &mut mapper, &mut frame_allocator);
+    // 在堆上分配一个数字
+    let heap_value = Box::new(41);
+    println!("heap_value at {:p}", heap_value);
 
-    // 通过新的映射将字符串 `New!`  写到屏幕上。
-    let page_ptr: *mut u64 = page.start_address().as_mut_ptr();
-    unsafe { page_ptr.offset(400).write_volatile(0x_f021_f077_f065_f04e)};
+    // 创建一个动态大小的向量
+    let mut vec = Vec::new();
+    for i in 0..500 {
+        vec.push(i);
+    }
+    println!("vec at {:p}", vec.as_slice());
+
+    // 创建一个引用计数的向量 -> 当计数达到0时将被释放
+    let reference_counted = Rc::new(vec![1, 2, 3]);
+    let cloned_reference = reference_counted.clone();
+    println!("current reference count is {}", Rc::strong_count(&cloned_reference));
+    core::mem::drop(reference_counted);
+    println!("reference count is {} now", Rc::strong_count(&cloned_reference));
+
+    //// 映射未使用的页
+    // let page = Page::containing_address(VirtAddr::new(0xdeadbeaf000));
+    // memory::create_example_mapping(page, &mut mapper, &mut frame_allocator);
+
+    // // 通过新的映射将字符串 `New!`  写到屏幕上。
+    // let page_ptr: *mut u64 = page.start_address().as_mut_ptr();
+    // unsafe { page_ptr.offset(400).write_volatile(0x_f021_f077_f065_f04e)};
     
     #[cfg(test)]
     test_main();
